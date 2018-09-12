c776cb38e6291e0abfff1f2a638d70c16b41d24f
Kazuya Takami <foundationmeister@outlook.com>
2018-09-12 17:04:34 +0900

    Review commit.

--- calculation.js
+++ calculation.js
@@ -11,7 +11,7 @@ $(function() {
     $reset = $("#reset"),
     $calculation = $("#calculation");
 
-    setCalculation();
+    setArea();
 
     $addition.on("click", function() {
       setCalculation("Add");
@@ -22,24 +22,41 @@ $(function() {
     });
 
     $reset.on("click", function() {
-      setCalculation("reset");
+      setCalculation("Reset");
     });
 
+    function setArea() {
+      for(i = 0; i < 10; i++) {
+        $calculation.append($("<tr>").attr("id", "line" + i));
+        for(j = 0; j < 10; j++) {
+          if(i === 0 && j === 0) {
+            $("#line" + i).append($("<td>").attr("id", "kind"));
+          } else if(i === 0 && j !== 0) {
+            $("#line" + i).append($("<td>").text(j));
+          } else if (i !== 0 && j === 0) {
+            $("#line" + i).append($("<td>").text(i));
+          } else {
+            $("#line" + i).append($("<td>"));
+          }
+        }
+      }
+    }
+
     function setCalculation(kind) {
-      $calculation.empty();
       switch (kind) {
         case "Add":
           kindCalculation = "+";
           break;
         case "Mult":
-          kindCalculation = "*"
+          kindCalculation = "*";
           break;
         default:
           kindCalculation = "";
       }
-      for(i = 0; i < 10; i++) {
-        $calculation.append($("<tr id='line" + i + "'>"));
-        for(j = 0; j < 10; j++) {
+      $("#kind").text(kindCalculation);
+
+      for(i = 1; i < 10; i++) {
+        for(j = 1; j < 10; j++) {
           switch (kind) {
             case "Add":
               result = i + j;
@@ -47,20 +64,11 @@ $(function() {
             case "Mult":
               result = i * j;
               break;
+            case "Reset":
+              result = "";
+              break;
           }
-          if(i === 0 && j === 0) {
-            $("#line" + i).append($("<td>").text(kindCalculation));
-          } else if(i === 0 && j !== 0) {
-            $("#line" + i).append($("<td>").text(j));
-          } else if (i !== 0 && j === 0) {
-            $("#line" + i).append($("<td>").text(i));
-          } else {
-            if (kindCalculation === "") {
-              $("#line" + i).append($("<td>"));
-            } else {
-              $("#line" + i).append($("<td>").text(result));
-            }
-          }
+          $("#line" + i).find("td").eq(j).text(result);
         }
       }
     }
